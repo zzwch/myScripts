704639c60abf4bebd69dad340f15b67ab4a4c5c3
Hazard <lizc07@vip.qq.com>
2017-06-05 01:27:42 -0500

    Add supports for gencode GTF

--- tag-based_scPipe_human/main.sh
+++ tag-based_scPipe_human/main.sh
@@ -1,23 +1,41 @@
 pipelineDir="/home/lizongcheng/myScripts/smartseq_pipeline_human/"
 # $1 - workspace directory
 # $2 - file including a raw data file name per line
-# $3 - qsub mission nums under processing
-# $4 - barcode file
+# $3 - skip code 
+#      (trim, tophat_refgene, tophat_gencode, htseq_refgene, htseq_gencode;)
+#      0  - 0b000000 no skip
+#      1  - 0b000001 skip htseq_gencode
+#      2  - 0b000010 skip htseq_refgene
+#      4  - 0b000100 skip tophat_gencode
+#      8  - 0b001000 skip tophat_refgene
+#      16 - 0b010000 skip trim
+#      32 - 0b100000 skip split
+# $4 - qsub mission nums under processing
+# $5 - barcode file
 workdir=$1
 files=$2
 
 if [ $# -lt 3 ];then
-  processn=40
+  skipcode=$((0))
 else
-  processn=$3
+  skipcode=$3
 fi
 
 if [ $# -lt 4 ];then
+  processn=40
+else
+  processn=$4
+fi
+
+if [ $# -lt 5 ];then
   barcode="$pipelineDir/96-8bp-barcode.txt"
 else
-  barcode=$4
+  barcode=$5
 fi
+
+
+
 cat $files | while read sample
 do 
-  sh "${pipelineDir}/smartseq_steps.sh" $workdir $sample $processn $barcode
+  sh "${pipelineDir}/smartseq_steps.sh" $workdir $sample $skipcode $processn $barcode
 done
--- tag-based_scPipe_human/smartseq_steps.sh
+++ tag-based_scPipe_human/smartseq_steps.sh
@@ -3,8 +3,9 @@ source ~/.bash_profile
 pipelineDir="/home/lizongcheng/myScripts/smartseq_pipeline_human/"
 workdir=$1
 sample=$2
-processn=$3
-barcode_list=$4
+skipcode=$3
+processn=$4
+barcode_list=$5
 count=`less $barcode_list | wc -l`
 
 indir="$workdir/data_raw/"
@@ -22,6 +23,9 @@ genome="/data1/users/lizongcheng/genomes/Homo_sapiens/hg19_Tang/hg19_ERCC92_RGC"
 refGene_GTF="/data1/users/lizongcheng/genomes/Homo_sapiens/hg19_Tang/hg19_ERCC92_RGC.refGene.gff"
 py_umicount="${pipelineDir}/scripts/0.4.UMI_HTseq.py"
 
+gencodev19_index="/data1/users/lizongcheng/genomes/Homo_sapiens/hg19_Tang/hg19_ERCC92_RGC.gencodev19/gencode.v19.annotation"
+gencodev19_GTF="/data1/users/lizongcheng/genomes/Homo_sapiens/hg19_Tang/hg19_ERCC92_RGC.gencodev19/gencode.v19.annotation.gff"
+
 resdir="$workdir/out_results/"
 
 f_split(){
@@ -30,19 +34,43 @@ f_split(){
 
 }
 
-f_tophat_htseq_copy(){
-mkdir -p $resdir
+f_tophat_htseq_both(){
+mkdir -p $resdir/refGene/
+mkdir -p $resdir/gencode/
+
 for i in `seq 1 $count`; do 
+
+qsub_trim="perl $pl_trimtso $outdir/${sample}/$prefix$i/$prefix$i.UMI.fq.gz $outdir/$sample/$prefix$i/$prefix$i.UMI.trim.fq.gz 0 \n
+perl $pl_trim --indir $outdir/$sample/$prefix$i --outdir $outdir/$sample/$prefix$i --sample $prefix${i}.UMI.trim\n"
+qsub_tophat_refgene="tophat -p 8 --transcriptome-index $transcriptome_index --library-type fr-unstranded -o $outdir/$sample/$prefix$i/tophat_out_refgene $genome $outdir/$sample/$prefix$i/$prefix$i.UMI.trim.clean.fq.gz \n"
+qsub_htseq_refgene="samtools sort -n --output-fmt SAM -o $outdir/$sample/$prefix$i/tophat_out_refgene/accepted_hits.sort_name.sam $outdir/$sample/$prefix$i/tophat_out_refgene/accepted_hits.bam \n
+htseq-count -s no -f sam -o $outdir/$sample/$prefix$i/tophat_out_refgene/accepted_hits.count.sam $outdir/$sample/$prefix$i/tophat_out_refgene/accepted_hits.sort_name.sam $refGene_GTF > $outdir/$sample/$prefix$i/tophat_out_refgene/accepted_hits.count.txt \n
+python $py_umicount $outdir/$sample/$prefix$i/tophat_out_refgene/accepted_hits.count.sam $outdir/$sample/$prefix$i/tophat_out_refgene/accepted_hits.count.UMIcount.txt \n"
+qsub_tophat_gencode="tophat -p 8 --transcriptome-index $gencodev19_index --library-type fr-unstranded -o $outdir/$sample/$prefix$i/tophat_out_gencode $genome $outdir/$sample/$prefix$i/$prefix$i.UMI.trim.clean.fq.gz \n"
+qsub_htseq_gencode="samtools sort -n --output-fmt SAM -o $outdir/$sample/$prefix$i/tophat_out_gencode/accepted_hits.sort_name.sam $outdir/$sample/$prefix$i/tophat_out_gencode/accepted_hits.bam \n
+htseq-count -s no -f sam -o $outdir/$sample/$prefix$i/tophat_out_gencode/accepted_hits.count.sam $outdir/$sample/$prefix$i/tophat_out_gencode/accepted_hits.sort_name.sam $gencodev19_GTF > $outdir/$sample/$prefix$i/tophat_out_gencode/accepted_hits.count.txt \n
+python $py_umicount $outdir/$sample/$prefix$i/tophat_out_gencode/accepted_hits.count.sam $outdir/$sample/$prefix$i/tophat_out_gencode/accepted_hits.count.UMIcount.txt \n"
+qsub_copy_refgene="cp $outdir/$sample/$prefix$i/tophat_out_refgene/align_summary.txt $resdir/refGene/$prefix$i.align_summary.txt \n
+cp $outdir/$sample/$prefix$i/tophat_out_refgene/accepted_hits.count.UMIcount.txt $resdir/refGene/$prefix$i.UMIcount.txt \n"
+qsub_copy_gencode="cp $outdir/$sample/$prefix$i/tophat_out_gencode/align_summary.txt $resdir/gencode/$prefix$i.align_summary.txt \n
+cp $outdir/$sample/$prefix$i/tophat_out_gencode/accepted_hits.count.UMIcount.txt $resdir/gencode/$prefix$i.UMIcount.txt \n"
+
+if [ $(($skipcode & 16)) -eq 16 ];then qsub_trim=''; fi
+if [ $(($skipcode & 8)) -eq 8 ];then qsub_tophat_refgene=''; fi
+if [ $(($skipcode & 4)) -eq 4 ];then qsub_tophat_gencode=''; fi
+if [ $(($skipcode & 2)) -eq 2 ];then qsub_htseq_refgene=''; fi
+if [ $(($skipcode & 1)) -eq 1 ];then qsub_htseq_gencode=''; fi
+
   myqsub -r -n $prefix$i -c "
-perl $pl_trimtso $outdir/${sample}/$prefix$i/$prefix$i.UMI.fq.gz $outdir/$sample/$prefix$i/$prefix$i.UMI.trim.fq.gz 0 \n
-perl $pl_trim --indir $outdir/$sample/$prefix$i --outdir $outdir/$sample/$prefix$i --sample $prefix${i}.UMI.trim\n
-tophat -p 8 --transcriptome-index $transcriptome_index --library-type fr-unstranded -o $outdir/$sample/$prefix$i/tophat_out $genome $outdir/$sample/$prefix$i/$prefix$i.UMI.trim.clean.fq.gz \n
-samtools sort -n --output-fmt SAM -o $outdir/$sample/$prefix$i/tophat_out/accepted_hits.sort_name.sam $outdir/$sample/$prefix$i/tophat_out/accepted_hits.bam \n
-htseq-count -s no -f sam -o $outdir/$sample/$prefix$i/tophat_out/accepted_hits.count.sam $outdir/$sample/$prefix$i/tophat_out/accepted_hits.sort_name.sam $refGene_GTF > $outdir/$sample/$prefix$i/tophat_out/accepted_hits.count.txt \n
-python $py_umicount $outdir/$sample/$prefix$i/tophat_out/accepted_hits.count.sam $outdir/$sample/$prefix$i/tophat_out/accepted_hits.count.UMIcount.txt \n
-cp $outdir/$sample/$prefix$i/tophat_out/align_summary.txt $resdir/$prefix$i.align_summary.txt \n
-cp $outdir/$sample/$prefix$i/tophat_out/accepted_hits.count.UMIcount.txt $resdir/$prefix$i.UMIcount.txt \n
-cp $outdir/$sample/$prefix$i/$prefix$i.UMI.trim.QC.log $resdir/" -d 1 -p 8 -m 10gb
+$qsub_trim\n
+$qsub_tophat_refgene\n
+$qsub_tophat_gencode\n
+$qsub_htseq_refgene\n
+$qsub_htseq_gencode\n
+$qsub_copy_refgene\n
+$qsub_copy_gencode\n
+cp $outdir/$sample/$prefix$i/$prefix$i.UMI.trim.QC.log $resdir/ \n
+" -d 1 -p 8 -m 10gb
   while [ `qstat | wc -l` -gt $processn ]
   do 
 	sleep 60
@@ -50,6 +78,10 @@ cp $outdir/$sample/$prefix$i/$prefix$i.UMI.trim.QC.log $resdir/" -d 1 -p 8 -m 10
 done
 }
 
+if [ $(($skipcode & 32)) -eq 32 ];then 
+  echo "skip split barcode!\n";
+else
+  f_split;
+fi
 
-f_split;
-f_tophat_htseq_copy;
+f_tophat_htseq_both;
