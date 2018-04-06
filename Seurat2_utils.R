1a85d19f589402daf66998f2ccb3625fa8bcc884
Hazard <lizc07@vip.qq.com>
2018-04-07 00:01:02 +0800

    Update Seurat2_utils.R

--- Seurat2_utils.R
+++ Seurat2_utils.R
@@ -1,4 +1,61 @@
-AddCCscore <- function(pbmc, org = c("mouse","human"), gene.max = NULL, use.scale = F){
+pairwise.topgene.corr <- function(dataMat, ngene = 500, method = "spearman"){
+  order_gene <- apply(dataMat, 2, function(x){order(x, decreasing = T)[1:ngene]})
+  resMat <- matrix(NA, nrow = ncol(dataMat), ncol = ncol(dataMat), dimnames = list(colnames(dataMat), colnames(dataMat)))
+  for(i in 1:ncol(dataMat)){
+    for(j in i:ncol(dataMat)){
+      if(i == j){
+        resMat[i,j] <- 0
+      }else{
+        data.x <- dataMat[,i,drop = T]
+        data.y <- dataMat[,j,drop = T]
+        sel_gene <- unique(c(order_gene[,i], order_gene[,j]))
+        resMat[i,j] <- (1-cor(data.x[sel_gene], data.y[sel_gene], method = method))/2
+        resMat[j,i] <- resMat[i,j]
+      }
+    }
+  }
+  return(resMat)
+}
+
+pairwise.topgene.equal <- function(dataMat, ngene = 500){
+  order_gene <- apply(dataMat, 2, function(x){order(x, decreasing = T)[1:ngene]})
+  resMat <- matrix(NA, nrow = ncol(dataMat), ncol = ncol(dataMat), dimnames = list(colnames(dataMat), colnames(dataMat)))
+  for(i in 1:ncol(dataMat)){
+    for(j in i:ncol(dataMat)){
+      if(i == j){
+        resMat[i,j] <- 0
+      }else{
+        equal <- length(intersect(order_gene[,i], order_gene[,j]))
+        resMat[i,j] <- 1 - equal/(2*ngene-equal)
+        resMat[j,i] <- resMat[i,j]
+      }
+    }
+  }
+  return(resMat)
+}
+
+
+focusTSNEPlot <- function(object, group.by = 'orig.ident', focus = NULL, each = F, colors.use = NULL,...){
+  cats <- sort(unique(object@meta.data[[group.by]]))
+  focus = intersect(focus, cats)
+  if(each){
+    sapply(focus, function(x){
+      TSNEPlot(object, group.by =group.by, ...)
+    })
+  } else{
+    if(is.null(names(colors.use)))
+      grey_ind <- which(!(cats %in% focus))
+    else
+      grey_ind <- which(!(names(colors.use) %in% focus))
+    colors.use[grey_ind] <- "grey"
+    top <- object@meta.data[[group.by]] %in% focus
+    toprow <- c(rownames(object@meta.data)[which(!top)],rownames(object@meta.data)[which(top)])
+    object@dr$tsne@cell.embeddings <- object@dr$tsne@cell.embeddings[toprow,]
+    TSNEPlot(object, group.by = group.by, colors.use = colors.use, ...)
+  }
+}
+
+AddCCscore <- function(pbmc, org = c("mouse","human"), gene.max = NULL, add.dr = F, use.scale = F){
   geneset1 <- c("Mcm5","Pcna","Tyms","Fen1","Mcm2","Mcm4","Rrm1","Ung","Gins2","Mcm6","Cdca7","Dtl","Prim1","Uhrf1","Cenpu","Hells","Rfc2","Rpa2","Nasp","Rad51ap1","Gmnn","Wdr76","Slbp","Ccne2","Ubr7","Pold3","Msh2","Atad2","Rad51","Rrm2","Cdc45","Cdc6","Exo1","Tipin","Dscc1","Blm","Casp8ap2","Usp1","Clspn","Pola1","Chaf1b","Brip1","E2f8")
   geneset2 <- c("Hmgb2","Cdk1","Nusap1","Ube2c","Birc5","Tpx2","Top2a","Ndc80","Cks2","Nuf2","Cks1b","Mki67","Tmpo","Cenpf","Tacc3","Fam64a","Smc4","Ccnb2","Ckap2l","Ckap2","Aurkb","Bub1","Kif11","Anp32e","Tubb4b","Gtse1","Kif20b","Hjurp","Cdca3","Hn1","Cdc20","Ttk","Cdc25c","Kif2c","Rangap1","Ncapd2","Dlgap5","Cdca2","Cdca8","Ect2","Kif23","Hmmr","Aurka","Psrc1","Anln","Lbr","Ckap5","Cenpe","Ctcf","Nek2","G2e3","Gas2l3","Cbx5","Cenpa")
   org = org[1]
@@ -12,9 +69,13 @@ AddCCscore <- function(pbmc, org = c("mouse","human"), gene.max = NULL, use.scal
   
   pbmc@meta.data$G1S.Score <- colMeans(use.data[geneset1, rownames(pbmc@meta.data)])
   pbmc@meta.data$G2M.Score <- colMeans(use.data[geneset2, rownames(pbmc@meta.data)])
+  if(add.dr){
+    pbmc@dr$ccscore <- pbmc@dr$pca
+    pbmc@dr$ccscore@cell.embeddings <- as.matrix(pbmc@meta.data[,c("G1S.Score","G2M.Score")])
+  }
   pbmc
 }
-AddAVscore <- function(pbmc, org = c("mouse","human"), gene.max = NULL, use.scale = F){
+AddAVscore <- function(pbmc, org = c("mouse","human"), gene.max = NULL, add.dr = F, use.scale = F){
   geneset1 <- c("Efnb2","Dll4","Hey1","Gja4","Unc5b")
   geneset2 <- c("Ephb4","Nr2f2","Nrp2","Aplnr","Flt4")
   org = org[1]
@@ -28,6 +89,10 @@ AddAVscore <- function(pbmc, org = c("mouse","human"), gene.max = NULL, use.scal
   
   pbmc@meta.data$Artery.Score <- colMeans(use.data[geneset1, rownames(pbmc@meta.data)])
   pbmc@meta.data$Venous.Score <- colMeans(use.data[geneset2, rownames(pbmc@meta.data)])
+  if(add.dr){
+    pbmc@dr$avscore <- pbmc@dr$pca
+    pbmc@dr$avscore@cell.embeddings <- as.matrix(pbmc@meta.data[,c("Artery.Score","Venous.Score")])
+  }
   pbmc
 }
 myGenePlot <- function(pbmc, gene1, gene2, use.scaled = F, group.by = NULL, cols.use = colors(),...){
@@ -35,22 +100,38 @@ myGenePlot <- function(pbmc, gene1, gene2, use.scaled = F, group.by = NULL, cols
   plot(FetchData(pbmc, vars.all = gene1, use.scaled = use.scaled)[,1], FetchData(pbmc, vars.all = gene2,use.scaled = use.scaled)[,1],
        pch = 19, cex=1.6, col = cols.use[as.factor(group.colors)], xlab = gene1, ylab = gene2, ...)
 }
-
-myFeaturePlot <- function(object, features.plot, nrow = NULL, ncol = NULL, ...){
+myFeaturePlot <- function(object, features.plot, nrow = NULL, ncol = NULL, dr = c("tsne","pca"),...){
   require(ggplot2)
   require(gridExtra)
-  ggData <- as.data.frame(cbind(object@dr$tsne@cell.embeddings,FetchData(object, features.plot)))
-  colnames(ggData) <- c(colnames(object@dr$tsne@cell.embeddings),gsub("-",".",features.plot))
+  dr <- dr[1]
+  ggData <- as.data.frame(cbind(object@dr[[dr]]@cell.embeddings,FetchData(object, features.plot)))
+  colnames(ggData) <- c(colnames(object@dr[[dr]]@cell.embeddings),gsub("-",".",features.plot))
   # print(feature.tmp)
   # ggData[,feature.tmp] <- t(object@data[feature.tmp,])
+  if(dr == 'tsne') {
+    xx <- "tSNE_1"
+    yy <- "tSNE_2"
+  }
+  if(dr == 'pca'){
+    xx <- "PC1"
+    yy <- "PC2"
+  }
+  if(dr == 'ccscore'){
+    xx <- "G1S.Score"
+    yy <- "G2M.Score"
+  }
+  if(dr == 'avscore'){
+    xx <- "Artery.Score"
+    yy <- "Venous.Score"
+  }
   ggl <- lapply(features.plot, function(feature){
-    ggplot(ggData) + geom_point(mapping = aes_string(x = "tSNE_1", y = "tSNE_2", color = gsub("-",".",feature)), size = 2) + 
+    ggplot(ggData) + geom_point(mapping = aes_string(x = xx, y = yy, color = gsub("-",".",feature)), size = 2) + 
       scale_color_gradientn(colours = c("grey","yellow","red")) + 
       theme(legend.title = element_blank(),axis.title = element_blank()) + ggtitle(feature) 
   })
   grid.arrange(grobs = ggl, nrow= nrow,ncol = ncol)
 }
-
+                                                          
 bpGOEnrich <- function(pbmc.markers){
   require(BiocParallel)
   
