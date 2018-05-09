cae7be84d114864abd9e0a5e933d30ccedbe8984
Hazard <lizc07@vip.qq.com>
2018-05-09 11:00:17 +0800

    Add files via upload

---
+++ pipeline/smartliu/smartliu.py
@@ -0,0 +1,181 @@
+import click
+import ConfigParser
+import logging
+import os, re  
+from multiprocessing import cpu_count
+
+@click.command(options_metavar='-c config',
+    short_help='smartliu')
+@click.option('-c','--config', metavar='CONFIG', nargs=1, required=True,
+    type= click.File(mode='r', encoding=None, errors='strict', lazy=None, atomic=False),
+    help = 'configuration file; Required')
+@click.version_option()
+
+def configRead(config):
+    cf = ConfigParser.ConfigParser()
+	cf.read(config)
+	
+def steps(input, output, genome, steps, parallel):
+    """
+    A simple command line tool for tag-based scRNA-Seq (from TangLab) data analysis.
+    Support for paired-end, illumina 1.9+ phred33
+    \b
+    NOTICE: 
+       See default.config file for more configuration information.
+    """
+	configRead
+    if os.path.isdir(output):
+        click.echo('OUTPUT directory exists already! May you wanna try to resume it, otherwise remove/delete it mannually firstly!')
+        exit()
+
+    os.makedirs(output)
+    flog = open(os.path.join(output, 'chipliu.log'), "w")
+    flog.write("A simple command line tool for chipseq data analysis.\n")
+    flog.close()
+ 
+    logging.basicConfig(filename = os.path.join(output, 'chipliu.log'), 
+        level = logging.INFO, filemode = 'a', format = '%(asctime)s - %(levelname)s: %(message)s')
+    #logging.debug('debug')
+    #logging.info('info')
+    #logging.warning('warn')
+    #logging.error('error')
+
+    logging.info('STEP0 - check parameters')
+    # check_options(input, output, genome, steps, parallel)
+    # check input paired-end fastaq
+    logging.info(' '*2 + 'check INPUT: paired-end fastq files for each sample')
+    logging.info(' '*4 + input)
+    samples = os.listdir(input)
+    sampleinfos = {}
+    re_fastq = re.compile('^(.*)[-_\.]r?(read)?[12]\.f.*q(\.gz)?$',re.I)
+    re_fastq1 = re.compile('^(.*)[-_\.]r?(read)?1\.f.*q(\.gz)?$',re.I)
+    re_fastq2 = re.compile('^(.*)[-_\.]r?(read)?2\.f.*q(\.gz)?$',re.I)
+    for s in samples:
+        s_dir = os.path.join(input,s)
+        if os.path.isdir(s_dir):
+            #logging.info(s_dir)
+            s_files = os.listdir(s_dir)
+            s_fastq1 = {}
+            s_fastq2 = {}
+            for s_file in s_files:
+                if os.path.isfile(s_file):
+                    s_re1 = re_fastq1.match(s_file)
+                    s_re2 = re_fastq2.match(s_file)
+                    if s_re1 != None:
+                        s_fastq1[s_re1.group(1)] = s_file
+                    elif s_re2 != None:
+                        s_fastq2[s_re2.group(1)] = s_file
+            for s_fastq in list(set(s_fastq1.keys()).intersection(set(s_fastq2.keys()))):
+                sampleinfos[s] = [s_fastq1[s_fastq], s_fastq2[s_fastq]]
+                logging.info(' '*6 + s)
+                logging.info(' '*8 + s_fastq1[s_fastq])
+                logging.info(' '*8 + s_fastq2[s_fastq])
+    # check output existence
+    logging.info(' '*2 + 'check OUTPUT: existence')
+    if os.path.isdir(output):
+        logging.info(' '*4 + output + ': OUTPUT directory has been created!')
+    else:
+        logging.info(' '*4 + ': OUTPUT directory cannot be created!?')
+        exit()
+    # check genome existence
+    logging.info(' '*2 + 'check INDEX: existence')
+    if (os.path.isfile(genome+'.1.bt2') and os.path.isfile(genome+'.2.bt2') and os.path.isfile(genome+'.3.bt2') and os.path.isfile(genome+'.4.bt2')):
+        logging.info(' '*4 + 'genome bowtie2 INDEX files exists!')
+    else:
+        logging.info(' '*4 + 'genome bowtie2 INDEX files do not exist! Please build them first!')
+        #exit()
+    # check steps 
+    logging.info(' '*2 + 'check ANALYSIS_STEP: legality')
+    logging.info(' '*4 + 'these analysis will be performed sequentially: qc, mapping, post_mapping, peak_calling, normalization')
+    # check parallel
+    logging.info(' '*2 + 'check N_CPUS: maximum')
+    if (parallel > cpu_count()):
+        logging.info(' '*4 + 'N_CPUS exceed maximum core numbers! Number '+ str(int(0.8*int(cpu_count()))) + ' is recommended if available!')
+        exit()
+    else:
+        logging.info(' '*4 + str(parallel) + ' cores will be used for next analyses.')
+    
+    
+    logging.info('STEP0 - END')
+    
+    ############################
+    psamples = sampleinfos.keys()
+    logging.info('Samples to be processed:\n' + ','.join(sampleinfos.keys()))
+    ret = os.symlink(input, os.path.join(output, 'rawdata'))
+    if(ret !=0 ):
+        logging.warning('cannot create the link of input in the output dir!')
+        exit()
+    
+    clean_dir = os.path.join(output, 'clean_data')
+    qc_dir = os.path.join(output, 'clean_fastqc')
+    mapping_dir = os.path.join(output, 'mapping_results')
+    peak_dir = os.path.join(output, 'peak_results')
+    
+    os.mkdir(clean_dir)
+    os.mkdir(qc_dir)
+    os.mkdir(mapping_dir)
+    os.mkdir(peak_dir)
+    
+    for p in psamples:
+        p_input_dir = os.path.join(input,p)
+        p_clean_dir = os.path.join(clean_dir, p)
+        p_qc_dir = os.path.join(qc_dir, p)
+        p_mapping_dir = os.path.join(mapping_dir, p)
+        p_peak_dir = os.path.join(peak_dir, p)
+        
+        logging.info(p + ' >>> proceesing start...')
+        
+        #trim_galore
+        os.mkdir(p_clean_dir)
+        os.mkdir(p_qc_dir)
+        logging.info(p+' >>> trim_galore: cleaning data (trim adapter and remove low quality) and fastqc')
+        p_fq = [os.path.join(p_input_dir,rv) for r in zip(sampleinfos[p][1],sampleinfos[p][2]) for rv in r]
+        p_cmd_qc = 'trim_galore --fastqc --fastqc_args "-t %d -o %s" --stringency 10 --length 30 --max_n 15 --trim-n -o %s --paired %s' %(parallel, p_qc_dir, p_clean_dir, ' '.join(p_fq))
+        p_ret = os.system(p_cmd_qc)
+        if (p_ret != 0):
+            logging.warning(p+' is skipped, '+'due to nonzero return!')
+            continue
+        
+        #bowtie2
+        os.mkdir(p_mapping_dir)
+        logging.info(p+' >>> bowtie2: clean reads mapping to genome')
+        re_fq_val_1 = re.compile('^(.*)[-_\.]r?(read)?1_val_1\.f.*q(\.gz)?$',re.I)
+        re_fq_val_2 = re.compile('^(.*)[-_\.]r?(read)?2_val_2\.f.*q(\.gz)?$',re.I)
+        p_fq_val_1 = [os.path.join(p_clean_dir, r) for r in re.fq_val_1.match(os.listdir(p_clean_dir))]
+        p_fq_val_2 = [os.path.join(p_clean_dir, r) for r in re.fq_val_2.match(os.listdir(p_clean_dir))]
+        p_cmd_bowtie2 = 'bowtie2 -p %s -x %s -1 %s -2 %s -S %s > %s 2>&1' %(parallel, genome, ','.join(p_fq_val_1), ','.join(p_fq_val_2), os.path.join(p_mapping_dir, p+'.mapped.sam'), os.path.join(p_mapping_dir, p+'.mapping.log'))
+        p_ret = os.system(p_cmd_bowtie2)
+        if (p_ret != 0):
+            logging.warning(p+' is skipped, '+'due to nonzero return!')
+            continue
+        
+        # samtools
+        logging.info(p+' >>> samtools view: sam to bam')
+        p_cmd_samtools = 'samtools view -h -b -o %s %s' %(os.path.join(p_mapping_dir, p+'.mapped.bam'), os.path.join(p_mapping_dir, p+'.mapped.sam'))
+        p_ret = os.system(p_cmd_samtools)
+        if (p_ret != 0):
+            logging.warning(p+' is skipped, '+'due to nonzero return!')
+            continue
+        logging.info(p+' >>> samtools sort: sort bam')
+        p_cmd_samtools = 'samtools sort -o %s %s' %(os.path.join(p_mapping_dir, p+'.mapped.sorted.bam'), os.path.join(p_mapping_dir, p+'.mapped.bam'))
+        p_ret = os.system(p_cmd_samtools)
+        if (p_ret != 0):
+            logging.warning(p+' is skipped, '+'due to nonzero return!')
+            continue
+        logging.info(p+' >>> samtools view: get unique mapped bam')
+        p_cmd_samtools = 'samtools view -h -b -q 30 -o %s %s' %(os.path.join(p_mapping_dir, p+'.mapped.sorted.uniq.bam'), os.path.join(p_mapping_dir, p+'.mapped.sorted.bam'))
+        p_ret = os.system(p_cmd_samtools)
+        if (p_ret != 0):
+            logging.warning(p+' is skipped, '+'due to nonzero return!')
+            continue
+        logging.info(p+' >>> samtools rmdup: remove duplicates')
+        p_cmd_samtools = 'samtools rmdup %s %s >> %s' %(os.path.join(p_mapping_dir, p+'.mapped.sorted.uniq.bam'), os.path.join(p_mapping_dir, p+'.mapped.sorted.uniq.dedup.bam'), os.path.join(p_mapping_dir, p+'.mapping.log'))
+        p_ret = os.system(p_cmd_samtools)
+        if (p_ret != 0):
+            logging.warning(p+' is skipped, '+'due to nonzero return!')
+            continue
+        
+        # macs2
+        
+if __name__ == '__main__':
+    steps()
