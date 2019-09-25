fb1824796d0443ffa4e105093441f5ba990333cf
Hazard <lizc07@vip.qq.com>
2019-09-25 16:43:13 +0800

    corner ready

--- data_utils.R
+++ data_utils.R
@@ -1,16 +1,43 @@
-'%strin%' <- function(x, y){
-  
+'%strin%' <- function(x, table, ignore.case = T){
+  unlist(sapply(x, function(i) {any(grepl(pattern = i, table, ignore.case = ignore.case))}))
 }
 
-corner <- function(x, n = c(5,5), pos = 'top-left') {
-  if(length(n) = 1) n <- rep(n, 2)
-  nr <- min(n, nrow(x))
-  nc <- min(n, ncol(x))
-  if(pos %in% levels(interaction(c('top', 'middle', 'bottom'),
-                   c('left', 'right', 'center'), 
-                   sep = '-', lex.order = T))){
-      
+corner <- function(mat, n = c(5,5), pos = 'top-left'){
+  if(length(n) == 1) n <- rep(n, 2)
+  nr <- min(n, nrow(mat))
+  nc <- min(n, ncol(mat))
+  ava_pos <- levels(interaction(c('top', 'middle', 'bottom'),
+                                c('left', 'right', 'center'), 
+                                sep = '-', lex.order = T))
+  if(pos %in% ava_pos){
+    if('top' %strin% pos){
+      nrs <- 1
+      nre <- nr+nrs-1
+    }
+    if('middle' %strin% pos){
+      nrs <- round((nrow(mat) - nr)/2) +1
+      nre <- nr+nrs-1
+    }
+    if('bottom' %strin% pos){
+      nre <- nrow(mat)
+      nrs <- nre-nr+1
+    }
+    
+    if('left' %strin% pos){
+      ncs <- 1
+      nce <- nc+ncs-1
+    }
+    if('center' %strin% pos){
+      ncs <- round((ncol(mat) - nc)/2) +1
+      nce <- nc+ncs-1
+    }
+    if('right' %strin% pos){
+      nce <- ncol(mat)
+      ncs <- nce-nc+1
+    }
+  }else{
+    stop(paste0('available position is ', paste0(ava_pos, collapse = ", ")))
   }
-
-  x[1:min(n, nrow(x)), 1:min(n, ncol(x)]
+  
+  return(mat[nrs:nre, ncs:nce])
 }
